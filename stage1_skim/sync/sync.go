3dcd25083df1b36149e7afcad177466aec4ac7c6
Chris Baumbauer <cab@cabnetworks.net>
2017-02-06 14:08:59 -0800

    Fixed bug with output from services, and botched env setup

--- stage1_skim/sync/sync.go
+++ stage1_skim/sync/sync.go
@@ -14,34 +14,71 @@
 
 package main
 
-// Program used to orchestrate w/ other apps in the pod.  Accept a single
-// argument to reflect the service name of the first
+// Program used to orchestrate w/ other apps in the pod, aggregate the output
+// and dump things out to stdout
+// Accept a single argument to reflect the service name of the first
 
 import (
 	"fmt"
+    "io"
 	"os"
+    "path/filepath"
 	"os/signal"
 	"syscall"
 )
 
+func dumpOutput(f *os.File) {
+    for ;; {
+        _, err := io.Copy(os.Stdout, f)
+        if (err != nil) && (err == io.EOF) {
+            break
+        } else if err != nil {
+            fmt.Printf("Error encountered while reading from fifo: %q\n", err)
+            os.Exit(254)
+        }
+    }
+}
+
+func cleanup() {
+    c := make(chan os.Signal, 1)
+    signal.Notify(c, syscall.SIGUSR1)
+    _ = <-c
+
+    os.Exit(0)
+}
+
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Printf("ERROR: Expecting the service to start")
+	if len(os.Args) != 3 {
+		fmt.Printf("ERROR: Expecting the service to start as well as the pod basedir")
 		os.Exit(254)
 	}
 
+    /* Setup the fifo queue */
+    serverFifo := filepath.Join(os.Args[2], "sync.fifo")
+    err := syscall.Mkfifo(serverFifo, syscall.S_IRUSR|syscall.S_IWUSR|syscall.S_IWGRP)
+    if err != nil {
+        fmt.Printf("ERROR: Cannot create %s: %q\n", serverFifo, err)
+        os.Exit(254)
+    }
+
+    /* Get everything ready */
 	systemctlCmd := "/usr/bin/systemctl"
 	systemctlArgs := []string{systemctlCmd, "start", os.Args[1]}
+    go cleanup()
 
-	pid, err := syscall.ForkExec(systemctlCmd, systemctlArgs, nil)
+	_, err = syscall.ForkExec(systemctlCmd, systemctlArgs, nil)
 	if err != nil {
 		fmt.Printf("ERROR: Unable to start service: %q\n", err)
 		os.Exit(254)
 	}
 
-	fmt.Printf("Starting %s (%d)\n", os.Args[1], pid)
+    fd, err := os.Open(serverFifo)
+    if err != nil {
+        fmt.Printf("ERROR: Cannot open %s: %q\n", serverFifo, err)
+        os.Exit(254)
+    }
 
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, syscall.SIGINT)
-	_ = <-c
+    defer fd.Close()
+
+    dumpOutput(fd)
 }
